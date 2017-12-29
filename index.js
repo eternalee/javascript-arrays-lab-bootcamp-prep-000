78dae1de1ddc46c69a67d79821a9c05bf5df89ff
Eterna <one.eternity@gmail.com>
2017-12-29 22:17:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ function destructivelyAppendKitten(name) {
 }
 
 function destructivelyPrependKitten(name) {
-  
+  kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function 
